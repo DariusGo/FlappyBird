a4ea8f3d4e049d824e3f8f7270056d03be997421
Vasile <drumeavasilexd@gmail.com>
2018-06-17 20:29:36 +0300

    Add UI

--- FlappyBird/Assets/Scripts/Bird.cs
+++ FlappyBird/Assets/Scripts/Bird.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bird : MonoBehaviour {
+	
 	public float upForce = 200f;
+
 	private bool isDead = false;
 	private Rigidbody2D rb2d;
+	private Animator anim;
 
 	// Use this for initialization
 	void Start () { 
 		rb2d = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -18,11 +22,13 @@ public class Bird : MonoBehaviour {
 			if (Input.GetMouseButtonDown (0)) {
 				rb2d.velocity = Vector2.zero;
 				rb2d.AddForce (new Vector2(0, upForce));
+				anim.SetTrigger ("Flap");
 			}
 		}
 	}
 
 	void OnCollisionEnter2D () {
 		isDead = true;
+		anim.SetTrigger ("Die"); 
 	}
 }
